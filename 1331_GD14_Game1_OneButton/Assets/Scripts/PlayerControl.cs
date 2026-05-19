06656fcaab76d08fc4aabd4f9b41a4f535fe35f2
Epicjeong <yurijeong@icloud.com>
2026-05-18 21:10:02 -0700

    change of plans
    nothing too major, and not like too many things were made anywho

--- 1331_GD14_Game1_OneButton/Assets/Scripts/PlayerControl.cs
+++ 1331_GD14_Game1_OneButton/Assets/Scripts/PlayerControl.cs
@@ -5,64 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private Vector2 _startSpeed;
-    private Vector2 _speed;
-
-    private float _breath = 100f;
-    private bool _holdingBreath = false;
-    private bool _canMove = true;
+    [SerializeField] private Vector2 _speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _speed = _startSpeed;
+
     }
 
     
     void FixedUpdate()
     {
-        if (_holdingBreath && _breath > 0)
-        {
-            _breath--;
-            Debug.Log(_breath);
-            if (_breath == 0)
-            {
-                _holdingBreath = false;
-                StartCoroutine(PanickedBreathing());
-            }
-        }
-        else if (!_holdingBreath)
-        {
-            transform.Translate(_speed * Time.fixedDeltaTime);
-            if (_breath < 100)
-            {
-                _breath++;
-                Debug.Log(_breath);
-            }
-        }
+        transform.Translate(_speed * Time.fixedDeltaTime);
     }
 
-    public void Stop(InputAction.CallbackContext context)
+    public void SwitchDirections(InputAction.CallbackContext context)
     {
-        _speed = new Vector2(0, 0);
-        _holdingBreath = true;
-        if (context.canceled && _holdingBreath)
-        {
-            Continue();
-        }
-    }
-
-    private void Continue()
-    {
-        _holdingBreath = false;
-        _speed = _startSpeed;
-
-    }
-
-    private IEnumerator PanickedBreathing()
-    {
-        yield return new WaitWhile(() => _breath < 100);
-        _speed = _startSpeed;
-
+        _speed = _speed * -1;
     }
 }
