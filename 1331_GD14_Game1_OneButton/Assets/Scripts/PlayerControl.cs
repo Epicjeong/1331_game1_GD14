e29b7f5c0b290fe66ac6d4fad8e84979f2ba0b90
Epicjeong <yurijeong@icloud.com>
2026-05-19 16:00:23 -0700

    movement properly works now
    pressing nothing moves right, holding moves left

--- 1331_GD14_Game1_OneButton/Assets/Scripts/PlayerControl.cs
+++ 1331_GD14_Game1_OneButton/Assets/Scripts/PlayerControl.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private Vector2 _speed;
+    [SerializeField] private Vector2 _startSpeed;
+    private Vector2 _speed;
+
+    [SerializeField] private int _health;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _speed = _startSpeed;
     }
 
-    
+
     void FixedUpdate()
     {
         transform.Translate(_speed * Time.fixedDeltaTime);
@@ -21,6 +25,10 @@ public class PlayerControl : MonoBehaviour
 
     public void SwitchDirections(InputAction.CallbackContext context)
     {
-        _speed = _speed * -1;
+        _speed = -_startSpeed;
+        if (context.canceled)
+        {
+            _speed = _startSpeed;
+        }
     }
 }
