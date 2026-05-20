7d6f6b7f317f8fb55e2e6fea6be22068dfd57346
Epicjeong <yurijeong@icloud.com>
2026-05-19 23:14:21 -0700

    Imported art, candy now spawns
    Art sprites are now ready to use, and candy spawns but doesnt fall yet

---
+++ 1331_GD14_Game1_OneButton/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] _candyPrefab;
+    private int _spawnRange = 8;
+    private float _spawnInterval = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("DropCandies", 0, _spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DropCandies()
+    {
+        int candySpawned = Random.Range(0, _candyPrefab.Length);
+        Vector2 spawnPos = new Vector2(Random.Range(-_spawnRange, _spawnRange), transform.position.y);
+        Instantiate(_candyPrefab[candySpawned], spawnPos, transform.rotation);
+    }
+}
